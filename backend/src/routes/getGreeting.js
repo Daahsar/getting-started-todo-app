c53363a04b83e7888545d09283b5ca8b02fdd8ad
Rashaad <rpor133@wgu.edu>
2025-10-04 13:59:37 -0400

    commit

--- backend/src/routes/getGreeting.js
+++ backend/src/routes/getGreeting.js
@@ -1,7 +1,11 @@
-const GREETING = 'Hello world!';
+const GREETINGS = [
+    "Boom!",
+    "Check Out My To-Do App!",
+    "Ta-da!",
+];
 
 module.exports = async (req, res) => {
     res.send({
-        greeting: GREETING,
+        greeting: GREETINGS[ Math.floor( Math.random() *GREETINGS.length)],
     });
 };
